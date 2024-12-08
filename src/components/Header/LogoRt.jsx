86a353feba837d7245793772e079e377afaa5dbb
lyapkin <lyapkin992@gmail.com>
2024-12-08 20:01:10 +0500

    .

--- src/components/Header/LogoRt.jsx
+++ src/components/Header/LogoRt.jsx
@@ -4,7 +4,7 @@ import s from "./Header.module.scss";
 const LogoRt = () => {
   return (
     <div className={s.logoRt}>
-      <Image src="/images/logo-rt.jpg" fill />
+      <Image src="/images/logo-rt.jpg" fill priority />
     </div>
   );
 };
